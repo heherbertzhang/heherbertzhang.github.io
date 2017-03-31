c3b35ef3406053b6112fd9c68d500882137d717d
heherbertzhang <heherbert.zhang@mail.utoronto.ca>
2017-03-30 22:24:32 -0400

    Finish first round edit

---
+++ email_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+// FIXME: Update this to your desired email address.
+$recipient = "herbertzhang0512@gmail.com";
+
+// FIXME: Update subject if necessary.
+$subject = "New contact from $name";
