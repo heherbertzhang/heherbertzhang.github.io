66d4ab53c5cea8c865ffa04d0777260a94ca9f78
He Zhang <herbertzhang0512@gmail.com>
2023-12-31 23:01:27 -0800

    Update email_settings.php

--- email_settings.php
+++ email_settings.php
@@ -1,7 +1,7 @@
 <?php
 
 // FIXME: Update this to your desired email address.
-$recipient = "he@hezhang.me";
+$recipient = "he";
 
 // FIXME: Update subject if necessary.
 $subject = "New contact from $name";
